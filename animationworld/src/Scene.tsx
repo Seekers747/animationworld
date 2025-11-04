f0b91cddff0a3f9063cafef3059ab847479b5550
imraan <imraan.mohamedhoesein@student.rocva.nl>
2025-11-04 12:01:24 +0100

    package toegevoed

--- animationworld/src/Scene.tsx
+++ animationworld/src/Scene.tsx
@@ -3,19 +3,78 @@ import { OrbitControls, Box } from '@react-three/drei'
 
 export default function Scene() {
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} />
-      <Box args={[2, 6, 1]}>
-        <meshStandardMaterial color="beige" />
-      </Box>
-      <Box args={[2, 6, 1]} position={[0, 0, 3]}>
-        <meshStandardMaterial color="beige" />
-      </Box>
-      <Box args={[2, 1, 4]} position={[0, 0, 1.5]}>
-        <meshStandardMaterial color="beige" />
-      </Box>
-      <OrbitControls />
+    <Canvas camera={{ position: [20, 5, -10], fov: 75 }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} />
+        <group rotation={[0, Math.PI, 0]}> 
+            {/* First letter H */}
+            <group position={[0, 0, 0]}> 
+                <Box args={[2, 6, 1]}>
+                    <meshStandardMaterial color="green" />
+                </Box> 
+                <Box args={[2, 6, 1]} position={[0, 0, 3]}>
+                    <meshStandardMaterial color="green" /> 
+                </Box>
+                <Box args={[2, 1, 4]} position={[0, 0, 1.5]}>
+                    <meshStandardMaterial color="green" /> 
+                </Box>
+            </group> 
+            {/* Second letter E */} 
+            <group position={[0, 0, 5]}> 
+                <Box args={[2, 6, 1]}> 
+                    <meshStandardMaterial color="red" />
+                </Box> 
+                <Box args={[2, 1, 4]} position={[0, 0, 1.5]}>
+                    <meshStandardMaterial color="red" />
+                </Box>
+                <Box args={[2, 1, 4]} position={[0, 2.5, 1.5]}>
+                    <meshStandardMaterial color="red" />
+                </Box> 
+                <Box args={[2, 1, 4]} position={[0, -2.5, 1.5]}>
+                    <meshStandardMaterial color="red" />
+                </Box> 
+            </group>
+            {/* Third letter L */}
+            <group position={[0, 0, 10]}> 
+                <Box args={[2, 6, 1]}> 
+                    <meshStandardMaterial color="orange" />
+                </Box> 
+                <Box args={[2, 1, 4]} position={[0, -2.5, 1.5]}>
+                    <meshStandardMaterial color="orange" />
+                </Box> 
+            </group>
+            {/* Fourth letter L */}
+            <group position={[0, 0, 15]}> 
+                <Box args={[2, 6, 1]}> 
+                    <meshStandardMaterial color="purple" />
+                </Box> 
+                <Box args={[2, 1, 4]} position={[0, -2.5, 1.5]}>
+                    <meshStandardMaterial color="purple" />
+                </Box> 
+            </group>
+            {/* Fifth letter O */}
+            <group position={[0, 0, 20]}> 
+                <Box args={[2, 6, 1]}>
+                    <meshStandardMaterial color="pink" /> 
+                </Box> 
+                <Box args={[2, 6, 1]} position={[0, 0, 3]}>
+                    <meshStandardMaterial color="pink" /> 
+                </Box>
+                <Box args={[2, 1, 4]} position={[0, 2.5, 1.5]}>
+                    <meshStandardMaterial color="pink" /> 
+                </Box>
+                <Box args={[2, 1, 4]} position={[0, -2.5, 1.5]}>
+                    <meshStandardMaterial color="pink" /> 
+                </Box>
+            </group>
+            {/* Ground */}
+            <group position={[0, -3.5, 11.5]}> 
+                <Box args={[10, 1, 25]}> 
+                    <meshStandardMaterial color="lightblue" />
+                </Box> 
+            </group> 
+        </group>
+        <OrbitControls target={[0, 0, -10]} />
     </Canvas>
   )
 }
